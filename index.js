586290c30015503e05aaf735e959779531d5ee4e
SamirMamede <anuarsamir@gmail.com>
2024-05-13 23:40:05 -0400

    create config, controller, routes and fix index

--- index.js
+++ index.js
@@ -1,33 +1,10 @@
-const fs = require('fs');
-const wppconnect = require('@wppconnect-team/wppconnect');
+const express = require('express');
+const server = require('./config/server.js');
+const routes = require('./controllers/routes.js');
+const app = express();
 
-wppconnect
-  .create({
-    session: 'sessionName',
-    catchQR: (base64Qr, asciiQR) => {
-      console.log(asciiQR); // Optional to log the QR in the terminal
-      var matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/),
-        response = {};
+app.use('/', routes);
 
-      if (matches.length !== 3) {
-        return new Error('Invalid input string');
-      }
-      response.type = matches[1];
-      response.data = new Buffer.from(matches[2], 'base64');
-
-      var imageBuffer = response;
-      require('fs').writeFile(
-        'out.png',
-        imageBuffer['data'],
-        'binary',
-        function (err) {
-          if (err != null) {
-            console.log(err);
-          }
-        }
-      );
-    },
-    logQR: false,
-  })
-  .then((client) => start(client))
-  .catch((error) => console.log(error));
+app.listen(server.port, () => {
+  console.log(`Server started on port 3000 ${server.port}`)
+})
